5e56e03a9d7e1eaf5a77b2fcd520ea376780ef62
junpei.kajiwara <junpei.kaji.3821@gmail.com>
2021-05-20 14:06:53 +0000

    ユーザ登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "signup", to: "users#new"
+  post "users", to: "users#create"
+  
   root to: "tasks#index"
   resources :tasks
 end
